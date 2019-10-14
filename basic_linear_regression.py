2a6d64f71d732804917d2ce468c08fd24df7239e
lazrossi <lazrossi@student.42.fr>
2019-10-14 19:19:33 +0200

    basic graph with gradient descent

--- basic_linear_regression.py
+++ basic_linear_regression.py
@@ -1,11 +1,28 @@
 import csv
 import matplotlib.pyplot as plt
 
-#def estimatePrice(list_array):
-#    float theta = 0
-#    for f in list_array:
-#        theta += 
-    
+theta_zero = float(50 * 600) * (-1)
+theta_one  = float(0)
+precision  = float(0.1)
+
+def showOriginalData(data):
+    x_val = [x[0] for x in data]
+    y_val = [x[1] for x in data]
+    plt.xlabel(data_categories[0])
+    plt.ylabel(data_categories[1])
+    plt.plot(x_val, y_val)
+
+def estimatePrice(mileage):
+    return theta_zero + theta_one * mileage
+
+def getSquaredDeviationSum(data):
+    result = float(0.0)
+    for n in data:
+        result += (n[1] - estimatePrice(n[0])) * (n[1] - estimatePrice(n[0]))
+    return (result / len(data))
+
+def getSecondDegreeDerivative(value):
+    return
 
 if __name__ == '__main__':
     data = list(csv.reader(open("./data.csv")))
@@ -14,9 +31,16 @@ if __name__ == '__main__':
     data_sort = []
     for n in data:
         data_sort.append(tuple(map(int, n)))
-    x_val = [x[0] for x in data_sort]
-    y_val = [x[1] for x in data_sort]
+
+    data_set = []
+    for s in range(1200):
+        data_set.append(tuple([theta_zero, getSquaredDeviationSum(data_sort)]))
+        theta_zero += 50
+
+    x_val = [x[0] for x in data_set]
+    y_val = [x[1] for x in data_set]
     plt.xlabel(data_categories[0])
     plt.ylabel(data_categories[1])
     plt.plot(x_val, y_val)
+    showOriginalData(data_sort)
     plt.show()
