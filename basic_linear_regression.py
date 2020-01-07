46aa9ada7765affb56358707b34d31fb064c35f9
lazrossi <lazrossi@student.42.fr>
2020-01-07 14:26:42 +0100

    user loop working

--- basic_linear_regression.py
+++ basic_linear_regression.py
@@ -15,33 +15,27 @@ def parse():
 
 def userLoop(dataFields, data, regVal):
     dataSetRange        = getDataSetRange(data)
-    data                = normalizeData(data, dataSetRange)
+
     valueToEstimateX    = float(input("Input a " + dataFields[0] +
     ", and the program will estimate its corresponding " + dataFields[1] + " : "))
 
-    thetas = linearRegression(data, regVal)
-
+    thetas           = linearRegression(normalizeData(data, dataSetRange), regVal)
     valueToEstimateY = denormalizeY((normalizeX(valueToEstimateX, dataSetRange) * thetas[1] + thetas[0]), dataSetRange)
+
     print("For " + str(valueToEstimateX) + dataFields[0] + ", the estimated " + dataFields[1] + " is " + str(valueToEstimateY))
     showEstimatedPoint((valueToEstimateX, valueToEstimateY))
-    showOriginalDataPoints(denormalizeData(data, dataSetRange))
+
+    showOriginalDataPoints(data)
     abline(thetas[1], thetas[0], dataSetRange)
     show()
-
-
-    #data.append((valueToEstimateX, valueToEstimateY))
-    #userLoop(dataFields, data, regVal)
-
+    data.append((valueToEstimateX, valueToEstimateY))
+    userLoop(dataFields, data, regVal)
 
 def main():
     args                = parse()
     (dataFields, data)  = getData(args.data)
     regVal              = regressionValues(args.thetaZero, args.thetaOne, args.learningRate, args.iter)
     userLoop(dataFields, data, regVal)
-    
-    #regVal = linearRegression(data, regVal)
-    #thetas = (regVal.theta_zero, regVal.theta_one)
-    #
 
 if __name__ == '__main__':
     main()
